da4117d424a682dc470fc00d9429baef2e50453f
Abjik <malikatoken05@gmail.com>
2022-04-07 15:51:30 +0600

    добавил рабочие кнопки

--- back/swap.js
+++ back/swap.js
@@ -3,3 +3,29 @@ function load() {
         opacity: 1
     }, 1000);
 }
+
+function change(element) {
+    switch (element.innerHTML) {
+        case "IMG":
+            $(".textar").animate({
+                opacity: 0
+            }, 300);
+            $(".imgin").animate({
+                opacity: 1
+            }, 300);
+            break;
+
+            case "Text":
+            $(".textar").animate({
+                opacity: 1
+            }, 300);
+            $(".imgin").animate({
+                opacity: 0
+            }, 300);
+            break;
+    
+        default:
+            
+            break;
+    }
+}
