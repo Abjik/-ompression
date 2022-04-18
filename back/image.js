86e0b1dba553eeed12b7c96be37e342bf6c8027b
Abjik <malikatoken05@gmail.com>
2022-04-18 20:47:33 +0600

    1

--- back/image.js
+++
@@ -1,9 +0,0 @@
-field.view.value = `${canvas.width} x ${canvas.height}`;
-    
-button.download = name.replace(/\w+$/, 'png');
-
-file = blobFrom( canvas.toDataURL() );
-    
-field.size.value = (file.size / 1024).toFixed(2);
-
-button.href = URL.createObjectURL( file );
